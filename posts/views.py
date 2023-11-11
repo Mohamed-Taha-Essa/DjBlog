53c46fff608e03b03bbad591c02f4bc9fbb1ae01
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-11-12 00:57:32 +0200

    add list_post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def list_posts(request):
+    data = Post.objects.all()
+    
+
+
+    return render(request ,'posts/list_post.html',{"taha":data})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import list_posts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',list_posts )
 ]
 
 urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
