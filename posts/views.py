fda8dbd6211ce9134d9886a52053844a9edda589
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-11-14 23:23:34 +0200

    add creat_post

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
     data = Post.objects.all()
-    context ={"taha":data}
+    context ={"post_list":data}
     return render(request ,'posts/post_list.html',context)
 
 #what is type of request
@@ -15,3 +16,28 @@ def post_detail(request ,pk):
 
     context = {'post_detail' :post_data}
     return render(request ,'posts/post_detail.html' ,context)
+
+
+def creat_post(request):
+    if request.method =='POST':
+        form =PostForm(request.POST ,request.FILES) 
+        if form.is_valid():
+            myform =form.save(commit=False)
+            myform.author =request.user
+            myform.save()
+            return redirect('/posts/')
+
+    else:
+        form =PostForm()
+    return render(request ,'posts/creat_post.html' ,{'form':form})
+
+
+
+
+from django.views.generic import ListView ,DetailView
+class PostList(ListView):
+    model =  Post
+    # context_object_name ='posts'
+
+class PostDetail(DetailView):
+    model =Post
